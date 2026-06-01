a1bdd138104318a890759bd41df3f2a093be2d5d
Pasya <tangleroad0101@gmail.com>
2026-06-01 15:32:11 +0800

    add counter

--- TebakAngka/main.cpp
+++ TebakAngka/main.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <chrono>
 #include <ctime>
+#include <limits>
 
 using namespace std;
 
@@ -14,30 +15,62 @@ int main() {
 
     int angkaRahasia = (rand() % 100) + 1;
     int tebakan;
-    char konfirmasi;
+    int hitungPercobaan = 0;
+    int totalPercobaan = 0;
+    int totalMenang = 0;
+    char konfirmasi = 'y';
 
-    cout << "=== Game tebak angka (1-100) ===" << endl;
+    cout << "===== Game tebak angka (1-100) =====" << endl;
     cout << "Jika angka yang ditebak terlalu besar dari target akan meghasilkan kata 'Terlalu Besar'" << endl;
     cout << "dan sebaliknya jika lebih kecil dari target akan menghasilkan 'Terlalu Kecil'" << endl;
 
     do {
 
-        cout << "\n\nMasukkan angka: "; cin >> tebakan;
+        try{
+            cout << "\n\nMasukkan angka: "; cin >> tebakan;
 
-        if (tebakan > angkaRahasia) {
-            cout << "Terlalu besar!";
-        } else if (tebakan < angkaRahasia) {
-            cout << "Terlalu kecil1";
-        } else if (tebakan == angkaRahasia) {
-            cout << "Tebakan Benar!" << endl;
-            cout << "\nApakah ingin main lagi? (y/n): "; cin >> konfirmasi;
-            angkaRahasia = (rand() % 100) + 1;
-        } else {
-            cout << "Angka tidak valid";
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                throw string("Input harus berupa angka!");
+            }
+            totalPercobaan++;
+            hitungPercobaan++;
+
+            if (tebakan > angkaRahasia) {
+                cout << "Terlalu besar!";
+            } else if (tebakan < angkaRahasia) {
+                cout << "Terlalu kecil!";
+            } else if (tebakan == angkaRahasia) {
+                cout << "Tebakan Benar!" << endl;
+                cout << "Anda berhasil menebak dalam " << hitungPercobaan << " kali percobaan.\n";
+                totalMenang++;
+
+                cout << "\nApakah ingin main lagi? (y/n): "; 
+                cin >> konfirmasi;
+            }
+
+            if (konfirmasi != 'n') {
+                hitungPercobaan = 0;
+                angkaRahasia = (rand() % 100) + 1;
+            }
+
+        } catch (const string& pesanError) {
+            cout << pesanError << endl;
         }
- 
+        
     } while (konfirmasi != 'n');
 
+    cout << "\n===================================" << endl;
+    cout << "          GAME OVER                " << endl;
+    cout << "===================================" << endl;
+    cout << "Total seluruh tebakan : " << totalPercobaan << " kali" << endl;
+    cout << "Total game yang dimenangkan : " << totalMenang << " kali" << endl;
+    cout << "Terima Kasih sudah bermain!" << endl;
+    
+    std::cin.ignore(100, '\n');
+    std::cout << "Tekan Enter untuk keluar ...";
+    std::cin.get();
 
     return 0;
 }
